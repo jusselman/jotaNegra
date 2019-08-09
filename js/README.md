6da8af149b2620d8a63e282dfb9c09a060dd1480
jusselman <usselmanjosh@gmail.com>
2019-08-09 08:13:53 -0700

    further revisions

---
+++ js/README.md
@@ -0,0 +1,34 @@
+# Black Jack
+
+>> **Blackjack** is the American variant of a globally popular banking game known as Twenty-One, whose relatives include Pontoon and Vingt-et-Un. It is a comparing card game between one or more players and a dealer, where each player in turn competes against the dealer. Players do not compete against each other.
+
+source - https://en.wikipedia.org/wiki/Blackjack
+
+
+![Screenshot](/images/screenshot.png)
+
+## Technologies Uses:
+
+HTML, CSS, Javascript
+
+## Getting Started:
+
+>> The **objective** is to reach the highest amount of card value less than or equal to 21.
+A hand that goes about twenty one forfeits the game. The game begins with a Javascript 
+constructor generating a deck which is then randomized. Next, the cards are taken from 
+deck one at a time and used for gameplay.
+
+
+## Next Steps:
+
+1. Add a betting feature to make the game more competitive. 
+
+2. Create a scoring element totaling the points gained from each hand. 
+
+3. Enhance graphics by implementing a smoother transition to cards appearing.
+
+4. Refactor code to simplify the functions. 
+
+5. Implement an animation upon winning.
+
+
--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
+// Black Jack Version 3.1.5 //
+
+
 // constants //
 var playerHandCtn = document.getElementById('playerHandCtn');
 var dealerHandCtn = document.getElementById('dealerHandCtn');
 var playerScoreDisp = document.getElementById('playScore');
 var dealerScoreDisp = document.getElementById('dealScore');
+var mess = document.getElementById('m-row');
+
+
+
 
 
 // Constructor Class to generate deck //
@@ -19,6 +26,7 @@ class Deck {
         this.dealer = -1;
         this.playerTotal;
         this.dealerTotal;
+        this.temp = [];
     }
 
     makeDeck() { 
@@ -41,20 +49,19 @@ class Deck {
         
         for ( let s = 0; s < suits.length; s++) {
             for ( let i = 0; i < cardIds.length; i++) {
-                // for ( let v = 0; v < cardVal.length; v++) {
                 this.deck.push(card(suits[s], cardIds[i], cardVal[i]));
             }
         }
     }
 
-    // This displays the generated deck in console //
+// This displays the generated deck in console //
     consoleDeck() {
         for (let i = 0; i < this.deck.length; i++ ) {
             console.log(this.deck[i]); 
         }
     }
     
-    // Randomize deck //
+// Randomize deck //
     shuffleDeck() {
     let currentIdx = this.deck.length, tempVal, randIdx;
     
@@ -78,19 +85,14 @@ class Deck {
             this.calcScore();
     }
 
-    // Render function //
+// Render function //
     
-renderCard() {
+    renderCard() {
     
-        // remove card from deck and set it to variable
         let newDealerCard = this.deck.splice(0,1)[0];
-        // push new card to dealer's hand array
         this.dealerHand.push(newDealerCard);
-        // create new image
         let newDealerCardDisplay = new Image;
-        // set new image's source to the image of the card we put in the dealer's hand
         newDealerCardDisplay.src = newDealerCard.cardImg;
-        // append the card to the dealer hand container
         dealerHandCtn.appendChild(newDealerCardDisplay);
         console.log(this.dealerHand);
 
@@ -102,109 +104,120 @@ renderCard() {
             playerHandCtn.appendChild(newPlayerCardDisplay);
 }
 
-
 // Stay button switches turn to Dealer //
-stay() {
-
-    console.log(this);
-    
-    if (this.playerTotal > this.dealerTotal) {
-        console.log('Stay works');
+    stay() {
         
+        if (21 > this.dealerTotal) {
+            console.log('Stay works');
+
             let newDealerCard = this.deck.splice(0,1)[0];
-            this.dealerHand.push(newDealerCard);
-            let newDealerCardDisplay = new Image;
-            newDealerCardDisplay.src = newDealerCard.cardImg;
-            dealerHandCtn.appendChild(newDealerCardDisplay);
-            console.log(this.dealerHand);
+
+                this.dealerHand.push(newDealerCard);
+                let newDealerCardDisplay = new Image;
+                newDealerCardDisplay.src = newDealerCard.cardImg;
+                dealerHandCtn.appendChild(newDealerCardDisplay);
+                console.log(this.dealerHand);
+                this.calcScore();
+                
+        } else{
+            return;
+        }
     }
-}
 
 // Calculate Dealer Score and Player Score //
-calcScore() {
+    calcScore() {
 
-    // console.log(this.playerHand);
-    this.dealerTotal = 0;
-    this.playerTotal = 0;
+        this.dealerTotal = 0;
+        this.playerTotal = 0;
 
-    for(let i = 0; i < this.dealerHand.length; i++) {
-        this.dealerTotal += this.dealerHand[i].cardVal
+        for(let i = 0; i < this.dealerHand.length; i++) {
+            this.dealerTotal = this.dealerHand[i].cardVal + this.dealerTotal;
+            console.log(`dealertotal: ${this.dealerTotal}`);
+            
+        }
+        console.log("------------------")
+        console.log(this.dealerTotal);
+        for(let i = 0; i < this.playerHand.length; i++) {
+            this.playerTotal += this.playerHand[i].cardVal
+        }
+        console.log("------------------")
+        console.log(this.playerTotal);
+        
+        
+        if (this.dealerTotal > 21) {
+            this.playerScore++;  
+            console.log(`playerscroe updated`+this.playerScore);      
+            mess.innerHTML = 'Dealer busts!';
+        } else if (this.playerTotal > 21){
+            this.dealerScore++;
+            console.log(this.dealerScore);
+            mess.innerHTML = 'Player busts!';
+        } else if (this.playerTotal === 21 && this.dealerTotal === 21) {
+            mess.innerHTML = 'Tie goes to the house';
+            this.dealerScore++;  
+        } else if (this.playerTotal === 21) {
+            mess.innerHTML = 'Black Jack! You win!';
+            this.playerScore++;  
+        } else if (this.dealerTotal === 21) {
+            mess.innerHTML = 'Black Jack! You lose!';
+            this.dealerScore++;  
+        } 
     }
-    console.log("------------------")
-    console.log(this.dealerTotal);
-    for(let i = 0; i < this.playerHand.length; i++) {
-        this.playerTotal += this.playerHand[i].cardVal
-    }
-    console.log("------------------")
-    console.log(this.playerTotal);
-    
-
-    if (this.dealerTotal > 21) {
-        this.playerScore++;  
-        console.log(this.playerScore);      
-        alert('Dealer busts!');
-    } else if (this.playerTotal > 21){
-        this.dealerScore++;
-        console.log(this.dealerScore);
-        setTimeout(() => alert('Player busts!'), 100);
-    } else if (this.playerTotal === 21 && this.dealerTotal === 21) {
-        alert('Tie goes to the house')
-     } else if (this.playerTotal === 21) {
-          alert('Black Jack! You win!')
-      } else if (this.dealerTotal === 21) {
-          alert('Black Jack! You lose!')
-      }
-    // } else {
-    //     this.cleanBoard();
-    // }
-}
-
-
 
 // clears cards in play for next hand //
-cleanBoard() {
-    this.dealtCards.push(this.playerHand.splice(0,-1));
-    this.dealtCafds.push(this.dealerHand.splice(0,-1));
+    cleanBoard() {
 
-    if (this.deck < 10 && playerScore > dealerScore) {
-        alert('Congrats, you win!!!');
-    } else if (this.deck < 10 && playerScore < dealerScore) {
-        alert('GAME OVER!!');
-    } else {
+        while(playerHandCtn.firstChild) {
+            playerHandCtn.removeChild(playerHandCtn.firstChild);
+        }
+        while(dealerHandCtn.firstChild) {
+            dealerHandCtn.removeChild(dealerHandCtn.firstChild);
+        }
 
-    this.renderCard();
+        this.dealerHand = [];
+        this.playerHand = [];
+
+
+        document.getElementById('playScore').textContent = this.playerScore;
+        document.getElementById('dealScore').textContent = this.dealerScore;
+        
+        if (this.deck.length < 10 && playerScore > dealerScore) {
+            mess.innerHTML = 'Congrats, you win!!!';
+        } else if (this.deck.length < 10 && playerScore < dealerScore) {
+            mess.innerHTML = 'GAME OVER!!';
+        } else { 
+            this.init();
+            
+        }
     }
-}
 
 // init function to begin game //
 init() {
+
+        while(playerHandCtn.firstChild) {
+            playerHandCtn.removeChild(playerHandCtn.firstChild);
+        }
+        while(dealerHandCtn.firstChild) {
+            dealerHandCtn.removeChild(dealerHandCtn.firstChild);
+        }
+    
+        this.dealerHand = [];
+        this.playerHand = [];
+        mess.innerHTML = "";
+      
         while (this.dealerHand.length < 2 && this.playerHand.length < 2) {
             this.renderCard();
             this.renderCard();
             this.calcScore();
         }
-
     }
-
 }
     
-
-
-
-
-
+// Generate new Deck //
 deck = new Deck();
 deck.makeDeck();
 deck.shuffleDeck();
 deck.consoleDeck();
-// deck.deal();
-// deck.addScore();
-// deck.calcScore();
-// deck.renderCard();
-
-// inits first hand of game //
- 
-
 
 // cached elements //
 var dealer;
@@ -218,59 +231,3 @@ var btnStay = document.getElementById('btn-stay');
 var btnDeal = document.getElementById('btn-deal');
 var btnNew = document.getElementById('btn-new');
 
-
-
-
-
-// event listeners //
-// document.getElementById('')
-
-// functions //
-// function init() {
-//     scores = {
-//         d: 0,
-//         t: 0,
-//         p: 0
-//     };
-//     results = {
-//         p: 'r',
-//         c: 'r',
-//     };
-//     winner = null; // 'p', 't', 'c' // 
-//     render();
-// }
-
-// function render() {
-//     for (let score in scores ) {
-//         scoreEls[score].textContent = scores[score]
-//     }
-
-//     //render results //
-
-// };
-
-
-// let array1 = [1,2,3,4,5,6,7,8,9];
-// function shuffle(arr) {
-//     let newPos;
-//     for (let i = arr.length; i--;) {
-//         newPos = Math.floor(Math.random() * i + 1);
-//     }
-// }
-
-// var rando = Math.floor(Math.random() * 10) + 1;
-
-// // var shuffed = shuffle(array1);
-
-// // shuffle(array1);
-
-
-// function getSum(total, sum) {
-//     return total + sum;
-// }
-
-// var numbers = [15, 1000, 999999999];
-
-// var numNums = numbers.reduce(getSum, 0);
-
-// console.log(numNums);
